ff223ac296b8ec6851acef0819b312cae1f066e7
Sean <stprice01@outlook.com>
2022-08-21 21:22:00 +0100

    V1.0.1 Fixed left button not being recognised

--- SnakeGame/Button.cpp
+++ SnakeGame/Button.cpp
@@ -13,7 +13,7 @@ bool Button::isPressed()
 {
   buttonValue = analogRead(pin);                                                                // read the button pin output
   
-  if ((buttonValue >= lowerBound) && (buttonValue < upperBound))                                // if it is in the button range, set the button state HIGH
+  if ((buttonValue > lowerBound) && (buttonValue <= upperBound))                                // if it is in the button range, set the button state HIGH
     return true;
   else
     return false;                                                    
